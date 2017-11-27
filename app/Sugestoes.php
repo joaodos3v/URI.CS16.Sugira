a8f123e440eeb604cfb1218bb7e552a22fc57ecb
joao-vieira <joaovitorvv@gmail.com>
2017-11-27 21:41:08 -0200

    Teste Busca de Sugestões

--- app/Sugestoes.php
+++ app/Sugestoes.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Sugestoes extends Model
-{
-    //
+class Sugestoes extends Model {
+    
+	protected $fillable = ['id', 'descricao', 'status', 'endereco', 'numero', 'base64', 'genero_id', 'classificacao_id', 'cidade_id', 'user_criador_id'];
+
 }
