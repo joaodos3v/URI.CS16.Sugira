aff062ed078961cecbdb772e2a4e71a1f918acf7
joao-vieira <joaovitorvv@gmail.com>
2017-11-27 22:42:08 -0200

    Correções Finais

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -74,11 +74,11 @@ class ApiController extends Controller {
 
 
 		foreach ($sugestoes as $key => $value) {
-			// $classificacao 					= Classificacao::find($value->classificacao_id);
+			$classificacao 					= DB::table('classificacoes')->where('id', '=', $value->classificacao_id)->get();
 			// $genero 						= Generos::find($value->genero_id);
 			// $sugestoes[$key]->classificacao = $classificacao->descricao;
 			// $sugestoes[$key]->genero 		= $genero->descricao;
-			$sugestoes[$key]->genero 		= $value->classificacao_id;
+			$sugestoes[$key]->genero 		= $classificacao;
 		}
 
 		return response()->json( ['qtd' => count($sugestoes), 'result' => $sugestoes ] );
